ddc217b13ac07d5da226c3e442fef78c8c2d6ed0
rogers22 <ahexorange88@gmail.com>
2019-09-10 17:04:57 -0700

    fleshed out vars

--- app/src/main/java/cs301/birthdaycake/CakeModel.java
+++ app/src/main/java/cs301/birthdaycake/CakeModel.java
@@ -1,4 +1,9 @@
 package cs301.birthdaycake;
 
 public class CakeModel {
+    public boolean CandleIsLit = true;
+    public int NumCandles = 2;
+    public boolean hasFrosting = true;
+    public boolean hasCandles = true;
+    private 
 }
